cd0499e13503d3c755ba79fed2f449986f846e01
its-Saab <musaababs@gmail.com>
2021-05-24 16:34:19 +0200

    Import css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import '@react95/icons/icons.css';
-
+import "./styles/App.css";
 
 ReactDOM.render(
   <React.StrictMode>
