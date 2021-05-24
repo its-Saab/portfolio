763418b7806fdd17a6a3b700e46abec088137c79
its-Saab <musaababs@gmail.com>
2021-05-24 16:41:21 +0200

    Clean import list - add cursor pointer

--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -1,10 +1,9 @@
 //npm packages
 import React, { useContext } from "react";
 import DataContext from "../contexts/dataContext";
-import { TaskBar, List } from "@react95/core";
+import { TaskBar, List, Button } from "@react95/core";
 import styled from "styled-components";
 import { useClippy } from "@react95/clippy";
-import { Button } from "@react95/core";
 //project files
 import { startWebamp } from "../utils/startWebamp";
 
@@ -15,32 +14,47 @@ const Link = styled.a`
 
 function Taskbar() {
 	const { projectRepo, react95Repo } = useContext(DataContext).getProjectInfo();
-  const { clippy } = useClippy();
+	const { clippy } = useClippy();
 	return (
 		<>
-		<TaskBar
-			list={
-				<List>
-					<List.Item className="pointer" icon="mplayer_1_10_16x16_4bit">
-						<Link onClick={() => startWebamp()}>Media</Link>
-					</List.Item>
-					<List.Divider />
-					<List.Item className="pointer" icon="flying_windows_100_16x16_4bit">
-						<Link href={react95Repo} target="_blank">
-							Built with React95
+			<TaskBar
+				list={
+					<List>
+						<Link onClick={() => startWebamp()}>
+							<List.Item className="pointer" icon="mplayer_1_10_16x16_4bit">
+								Media
+							</List.Item>
 						</Link>
-					</List.Item>
-					<List.Divider />
-					<List.Item className="pointer" icon="mystify_your_mind_100_16x16_4bit">
-						<Link href={projectRepo} target="_blank">
-							Repo
-						</Link>
-					</List.Item>
-				</List>
-			}
+						<List.Divider />
+						<List.Item className="pointer" icon="flying_windows_100_16x16_4bit">
+							<Link href={react95Repo} target="_blank" rel="noopener noreferrer">
+								Built with React95
+							</Link>
+						</List.Item>
+						<List.Divider />
+						<List.Item
+							className="pointer"
+							icon="mystify_your_mind_100_16x16_4bit"
+						>
+							<Link href={projectRepo} target="_blank" rel="noopener noreferrer">
+								Repo
+							</Link>
+						</List.Item>
+					</List>
+				}
 			/>
-			<Button style={{position:"absolute", top:"0",right:"0"}} onClick={() => clippy.play('Wave')}>Hello Clippy!</Button>
-			</>
+			<Button
+				style={{
+					position: "absolute",
+					top: "0",
+					right: "0",
+					cursor: "pointer",
+				}}
+				onClick={() => clippy.play("Wave")}
+			>
+				Hello Clippy!
+			</Button>
+		</>
 	);
 }
 
